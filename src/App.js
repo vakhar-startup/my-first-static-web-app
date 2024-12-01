28c2c26b0c411576667fa4225fff0895a984413d
vakhar-startup <info@vakhar.co.uk>
2024-12-01 23:43:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 
 function App() {
   const value = 'Faheem';
@@ -8,3 +8,30 @@ function App() {
 
 export default App;
 
+*/
+
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ExpenseList from './components/ExpenseList';
+import AddExpense from './components/AddExpense';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/expenses" element={<ExpenseList />} />
+        <Route path="/add-expense" element={<AddExpense />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
