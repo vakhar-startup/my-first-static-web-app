eda80bda6ba5f3cce09a5c52377c06cb378d1781
vakhar-startup <info@vakhar.co.uk>
2024-12-07 14:54:31 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Welcome to Travel and Expense Management</h1>
+      <h1>Welcome to Sohai's Travel and Expense Management</h1>
       <p>
         Manage your travel plans and expenses efficiently. Use the navigation
         menu to explore the features like tracking expenses, adding new expenses, and viewing reports.
